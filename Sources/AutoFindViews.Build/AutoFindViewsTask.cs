fc37134cd945f8096c0be077c47488ddd291b4a6
Valball <val_jub@live.fr>
2017-10-06 20:21:25 +0200

    SetLastTimeWrite to avoid infinite check

--- Sources/AutoFindViews.Build/AutoFindViewsTask.cs
+++ Sources/AutoFindViews.Build/AutoFindViewsTask.cs
@@ -1,92 +1,93 @@
 ﻿namespace AutoFindViews.Build
 {
-	using System;
-	using Microsoft.Build.Framework;
-	using Microsoft.Build.Utilities;
-	using System.IO;
-	using System.Linq;
-	using System.Xml;
-	using Microsoft.Build.Evaluation;
-	using Newtonsoft.Json;
+    using System;
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+    using System.IO;
+    using System.Linq;
+    using System.Xml;
+    using Microsoft.Build.Evaluation;
+    using Newtonsoft.Json;
 
-	public class AutoFindViewsTask : Task
-	{
-		#region Properties
+    public class AutoFindViewsTask : Task
+    {
+        #region Properties
 
-		[Required]
-		public string Namespace { get; set; }
+        [Required]
+        public string Namespace { get; set; }
 
-		[Required]
-		public string ProjectPath { get; set; }
+        [Required]
+        public string ProjectPath { get; set; }
 
-		[Required]
-		public ITaskItem Source { get; set; }
+        [Required]
+        public ITaskItem Source { get; set; }
 
-		[Output]
-		public string OutputFile { get; set; }
+        [Output]
+        public string OutputFile { get; set; }
 
-		#endregion
+        #endregion
 
-		#region Fields
+        #region Fields
 
-		private Configuration configuration;
+        private Configuration configuration;
 
-		#endregion
+        #endregion
 
-		/// <summary>
-		/// Reads configuration (custom mappings, ...) from the optional file placed at the root of the referencing project.
-		/// </summary>
-		private void ReadConfig()
-		{
-			var projectFolder = Path.GetDirectoryName(this.ProjectPath);
-			var path = Path.Combine(projectFolder, "AutoFindViews.config");
-			this.configuration = Configuration.Load(path);
-			Log.LogMessage($"Custom mapping : {string.Join(", ", this.configuration.Mapping.Select(x => $"{x.Key}={x.Value}"))}");
-		}
+        /// <summary>
+        /// Reads configuration (custom mappings, ...) from the optional file placed at the root of the referencing project.
+        /// </summary>
+        private void ReadConfig()
+        {
+            var projectFolder = Path.GetDirectoryName(this.ProjectPath);
+            var path = Path.Combine(projectFolder, "AutoFindViews.config");
+            this.configuration = Configuration.Load(path);
+            Log.LogMessage($"Custom mapping : {string.Join(", ", this.configuration.Mapping.Select(x => $"{x.Key}={x.Value}"))}");
+        }
 
-		public override bool Execute()
-		{
-			if (!File.Exists(this.Source.ItemSpec))
-			{
-				Log.LogError(null, null, null, Source, 0, 0, 0, 0, $"file {this.Source} not found");
-				return false;
-			}
+        public override bool Execute()
+        {
+            if (!File.Exists(this.Source.ItemSpec))
+            {
+                Log.LogError(null, null, null, Source, 0, 0, 0, 0, $"file {this.Source} not found");
+                return false;
+            }
 
-			try
-			{
-				this.ReadConfig();
+            try
+            {
+                this.ReadConfig();
 
-				Log.LogMessage($"Loading xml : {this.Source.ItemSpec}");
+                Log.LogMessage($"Loading xml : {this.Source.ItemSpec}");
 
-				var mapper = new TypeMapper(this.configuration.Mapping);
+                var mapper = new TypeMapper(this.configuration.Mapping);
 
-				var rootGen = new LayoutRootGenerator(Namespace);
-				var layoutGen = new LayoutHolderGenerator(mapper, Namespace);
+                var rootGen = new LayoutRootGenerator(Namespace);
+                var layoutGen = new LayoutHolderGenerator(mapper, Namespace);
 
-				rootGen.Generate(this.OutputFile);
-				var result = layoutGen.Generate(this.Source.ItemSpec, this.OutputFile);
+                rootGen.Generate(this.OutputFile);
+                var result = layoutGen.Generate(this.Source.ItemSpec, this.OutputFile);
 
-				if (!result)
-				{
-					Log.LogMessage($"Skipping generation of {OutputFile} because there is not relevant change in {Source.ItemSpec}.");
-				}
+                if (!result)
+                {
+                    File.SetLastWriteTime(OutputFile, DateTime.Now);
+                    Log.LogMessage($"Skipping generation of {OutputFile} because there is not relevant change in {Source.ItemSpec}.");
+                }
 
-				return true;
-			}
+                return true;
+            }
 
-			catch (XmlException xe)
-			{
-				Log.LogMessage("Error (xml): {0}", xe.Message);
-				Log.LogError(null, null, null, Source, xe.LineNumber, xe.LinePosition, 0, 0, $"{xe.Message}");
+            catch (XmlException xe)
+            {
+                Log.LogMessage("Error (xml): {0}", xe.Message);
+                Log.LogError(null, null, null, Source, xe.LineNumber, xe.LinePosition, 0, 0, $"{xe.Message}");
 
-				return false;
-			}
-			catch (Exception e)
-			{
-				Log.LogMessage("Error: {0}", e.Message);
-				Log.LogError(null, null, null, Source, 0, 0, 0, 0, $"{e.Message}");
-				return false;
-			}
-		}
-	}
+                return false;
+            }
+            catch (Exception e)
+            {
+                Log.LogMessage("Error: {0}", e.Message);
+                Log.LogError(null, null, null, Source, 0, 0, 0, 0, $"{e.Message}");
+                return false;
+            }
+        }
+    }
 }
